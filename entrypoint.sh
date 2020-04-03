5ddcb1a657b223f241a33d324b323f796320ecf7
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-03 13:10:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ sed -i 's/:1000:100:/:'$PUID':100:/g' /etc/passwd
 if [ ! -e /var/www/html/index.php ] && [ ! -e /var/www/html/wp-includes/version.php ]; then
     echo "WordPress is missing, installing now."
     curl --silent -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php && \
-    sed -i "s/'DB_HOST', 'localhost'/'DB_HOST', '$DB_HOST'/g" /usr/src/wordpress/wp-config.php && \
+    sed -i "s/'DB_HOST', 'db'/'DB_HOST', '$DB_HOST'/g" /usr/src/wordpress/wp-config.php && \
     sed -i "s/'DB_NAME', 'wordpress'/'DB_NAME', '$DB_NAME'/g" /usr/src/wordpress/wp-config.php && \
     sed -i "s/'DB_USER', 'wordpress'/'DB_USER', '$DB_USER'/g" /usr/src/wordpress/wp-config.php && \
     sed -i "s/'DB_PASSWORD', 'wordpress'/'DB_PASSWORD', '$DB_PASSWORD'/g" /usr/src/wordpress/wp-config.php && \

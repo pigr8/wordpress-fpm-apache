5b000dcf762665f9c3b3c56c9b99c4f8ef84d8e3
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-03 11:51:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ sed -i 's/:1000:100:/:'$PUID':100:/g' /etc/passwd
 if [ ! -e /var/www/html/index.php ] && [ ! -e /var/www/html/wp-includes/version.php ]; then
     echo "WordPress is missing, installing now."
     curl --silent -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php && \
-    sed -i "s/localhost/$WORDPRESS_DB_HOST/g" /usr/src/wordpress/wp-config.php && \
-    sed -i "s/wordpress/$WORDPRESS_DB_NAME/g" /usr/src/wordpress/wp-config.php && \
-    sed -i "s/wordpress/$WORDPRESS_DB_USER/g" /usr/src/wordpress/wp-config.php && \
-    sed -i "s/wordpress/$WORDPRESS_DB_PASS/g" /usr/src/wordpress/wp-config.php && \
+    sed -i "s/'DB_HOST', 'localhost'/'DB_HOST', '$DB_HOST'/g" /usr/src/wordpress/wp-config.php && \
+    sed -i "s/'DB_NAME', 'wordpress'/'DB_NAME', '$DB_NAME'/g" /usr/src/wordpress/wp-config.php && \
+    sed -i "s/'DB_USER', 'wordpress'/'DB_USER', '$DB_USER'/g" /usr/src/wordpress/wp-config.php && \
+    sed -i "s/'DB_PASSWORD', 'localhost'/'DB_PASSWORD', '$DB_PASSWORD'/g" /usr/src/wordpress/wp-config.php && \
     chown -R nobody:users /usr/src/wordpress && \
     cp -R /usr/src/wordpress/* /var/www/html && \
     chown -R nobody:users /var/www/html

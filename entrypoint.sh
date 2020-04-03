9039518515ac1828c543dceea875c7e2b790e85d
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-03 16:44:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,11 @@ if [ ! -e /var/www/html/index.php ] && [ ! -e /var/www/html/wp-includes/version.
     chown -R nobody:users /var/www/html
     echo "Done, Wordpress is ready to use."
 fi
-
+# Unset compiled variables for PHP
+unset PHP_MD5 PHP_INI_DIR GPG_KEYS PHP_LDFLAGS PHP_SHA256 \
+      PHPIZE_DEPS PHP_URL PHP_EXTRA_CONFIGURE_ARGS SHLVL \
+      PHP_CFLAGS PHP_ASC_URL PHP_CPPFLAGS SUPERVISOR_ENABLED \
+      SUPERVISOR_PROCESS_NAME SUPERVISOR_GROUP_NAME
+# Unset user variables
 unset DB_HOST DB_NAME DB_USER DB_PASSWORD PUID TZ
 exec "$@"

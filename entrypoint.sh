b3afe3592f150d37ef19b6ab73690c2242a9e0aa
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-03 12:46:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -e /var/www/html/index.php ] && [ ! -e /var/www/html/wp-includes/version.
     sed -i "s/'DB_HOST', 'localhost'/'DB_HOST', '$DB_HOST'/g" /usr/src/wordpress/wp-config.php && \
     sed -i "s/'DB_NAME', 'wordpress'/'DB_NAME', '$DB_NAME'/g" /usr/src/wordpress/wp-config.php && \
     sed -i "s/'DB_USER', 'wordpress'/'DB_USER', '$DB_USER'/g" /usr/src/wordpress/wp-config.php && \
-    sed -i "s/'DB_PASSWORD', 'localhost'/'DB_PASSWORD', '$DB_PASSWORD'/g" /usr/src/wordpress/wp-config.php && \
+    sed -i "s/'DB_PASSWORD', 'wordpress'/'DB_PASSWORD', '$DB_PASSWORD'/g" /usr/src/wordpress/wp-config.php && \
     chown -R nobody:users /usr/src/wordpress && \
     cp -R /usr/src/wordpress/* /var/www/html && \
     chown -R nobody:users /var/www/html

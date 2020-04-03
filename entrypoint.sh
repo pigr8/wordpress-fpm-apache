7092a7aa638586db964e368ef351e7596e563c40
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-03 13:44:42 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,16 +19,16 @@ sed -i 's/:1000:100:/:'$PUID':100:/g' /etc/passwd
 if [ ! -e /var/www/html/index.php ] && [ ! -e /var/www/html/wp-includes/version.php ]; then
     echo "WordPress is missing, installing now."
 	if [[ "${DB_HOST:-db}" && "${DB_NAME:-wordpress}" && "${DB_USER:-wordpress}" && "${DB_PASSWORD:-wordpress}" ]]; then
-        echo "No configuration provided, installing Wordpress with Wizard support."
+            echo "No configuration provided, installing Wordpress with Wizard support."
 	    rm /usr/src/wordpress/wp-config.php /usr/src/wordpress/wp-secrets.php
 	else
-        echo "Data provided with -e variables, auto configuring Wordpress."
-        curl --silent -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php && \
-		sed -i "s/'DB_HOST', 'db'/'DB_HOST', '$DB_HOST'/g" /usr/src/wordpress/wp-config.php && \
-        sed -i "s/'DB_NAME', 'wordpress'/'DB_NAME', '$DB_NAME'/g" /usr/src/wordpress/wp-config.php && \
-        sed -i "s/'DB_USER', 'wordpress'/'DB_USER', '$DB_USER'/g" /usr/src/wordpress/wp-config.php && \
-        sed -i "s/'DB_PASSWORD', 'wordpress'/'DB_PASSWORD', '$DB_PASSWORD'/g" /usr/src/wordpress/wp-config.php && \
-    fi
+            echo "Data provided with -e variables, auto configuring Wordpress."
+            curl --silent -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php && \
+	    sed -i "s/'DB_HOST', 'db'/'DB_HOST', '$DB_HOST'/g" /usr/src/wordpress/wp-config.php && \
+            sed -i "s/'DB_NAME', 'wordpress'/'DB_NAME', '$DB_NAME'/g" /usr/src/wordpress/wp-config.php && \
+            sed -i "s/'DB_USER', 'wordpress'/'DB_USER', '$DB_USER'/g" /usr/src/wordpress/wp-config.php && \
+            sed -i "s/'DB_PASSWORD', 'wordpress'/'DB_PASSWORD', '$DB_PASSWORD'/g" /usr/src/wordpress/wp-config.php && \
+        fi
     chown -R nobody:users /usr/src/wordpress && \
     cp -R /usr/src/wordpress/* /var/www/html && \
     chown -R nobody:users /var/www/html
